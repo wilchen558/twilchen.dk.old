11da760000ab46944aeb3f33a331b379f7b7df24
Tobias Wilchen <twp@peytz.dk>
2017-03-31 11:39:17 +0200

    Cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -9,30 +9,3 @@ $(function(){
       loopCount: false
     });
 });
-
-var demoList = new Vue({
-
-  el: '#demoList',
-
-  data: {
-    currentBranch: 'dev',
-    items: null
-  },
-
-  created: function () {
-    this.fetchData();
-  },
-
-  methods: {
-    fetchData: function () {
-    var self = this;
-    $.get( "./projects.json", function( data ) {
-        self.items = data;
-        console.log(data);
-    });
-
-    }
-
-  }
-
-});
